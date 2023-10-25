9bc63120ec243d0521a53c95d0a1eb68eb9e818d
Roice32 <alexandru.butnaru32@yahoo.com>
2023-10-25 20:42:31 +0300

    'cargo fmt' fdw gdi fgs

--- lab03/prob5/src/main.rs
+++ lab03/prob5/src/main.rs
@@ -1,42 +1,42 @@
 #[derive(Debug)]
 
 enum OpinionError {
-    PureHate
+    PureHate,
 }
 
-fn favorite_case_style () -> Option<String> {
+fn favorite_case_style() -> Option<String> {
     Some(String::from("camelCase"))
 }
 
-fn appreciation_for_snake_case () -> Option<u8> {
+fn appreciation_for_snake_case() -> Option<u8> {
     None
 }
 
-fn appreciation_for_not_putting_the_opening_bracket_on_a_new_line () -> Option<u8> {
+fn appreciation_for_not_putting_the_opening_bracket_on_a_new_line() -> Option<u8> {
     None
 }
 
-fn his_opinion_on_rust () -> Result<String, OpinionError> {
+fn his_opinion_on_rust() -> Result<String, OpinionError> {
     let fav_case: String;
     match favorite_case_style() {
         Some(f) => fav_case = String::from(f),
-        None => fav_case = String::from("None in particular.")
+        None => fav_case = String::from("None in particular."),
     }
     let a1: u8;
     match appreciation_for_snake_case() {
         Some(x) => a1 = x,
-        None => a1=0
+        None => a1 = 0,
     }
     let a2: u8;
     match appreciation_for_not_putting_the_opening_bracket_on_a_new_line() {
         Some(x) => a2 = x,
-        None => a2=0 
+        None => a2 = 0,
     }
-    if a1+a2 < 1 {
+    if a1 + a2 < 1 {
         return Err(OpinionError::PureHate);
     }
 
-    let grade: f32 = (a1+a2)as f32/2 as f32;
+    let grade: f32 = (a1 + a2) as f32 / 2 as f32;
     let mut answer: String = String::from("");
     answer += "He prefers putting '{' on a newline and ";
     answer += &fav_case;
@@ -44,7 +44,6 @@ fn his_opinion_on_rust () -> Result<String, OpinionError> {
     answer += &grade.to_string();
     answer += "/10.";
     Ok(answer)
-
 }
 
 fn main() {
