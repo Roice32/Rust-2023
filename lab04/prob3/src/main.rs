ba076404d744bae8cd3641db8508333d5c7c8bf8
Roice32 <alexandru.butnaru32@yahoo.com>
2023-11-01 21:46:59 +0200

    Solved Prob3

---
+++ lab04/prob3/src/main.rs
@@ -0,0 +1,26 @@
+use std::{fs, io};
+
+fn main() -> Result<(), io::Error> {
+    let abbreviations = vec![
+        ("pentru", "pt"),
+        ("pentru", "ptr"),
+        ("domnul", "dl"),
+        ("doamna", "dna"),
+        ("You all ought not to have", "Y'ought'n't've"),
+    ];
+    let text = fs::read_to_string("src/text.txt")?;
+    let mut replaced: bool;
+    for word in text.split(" ") {
+        replaced = false;
+        for (long_form, short_form) in &abbreviations {
+            if word == short_form as &str {
+                print!("{} ", long_form);
+                replaced = true;
+            }
+        }
+        if !replaced {
+            print!("{} ", word);
+        }
+    }
+    Ok(())
+}
