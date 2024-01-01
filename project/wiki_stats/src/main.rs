c2d5ea81b87f069b34eb84dc6f29de5c6db83211
Roice32 <alexandru.butnaru32@yahoo.com>
2024-01-02 00:56:14 +0200

    Implemented reading archive data
    Implemented basic functionalities: archive opening, file iteration, reading JSON items & deserializing them.
    Passed general project requirements.

--- project/wiki_stats/src/main.rs
+++ project/wiki_stats/src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use serde_derive::Deserialize;
+use std::{fs::File, io::Read};
+use zip::read::ZipArchive;
+
+#[derive(Debug, Deserialize)]
+struct Article {
+    id: String,
+    text: String,
+    title: String,
+}
+
+fn main() -> Result<()> {
+    let dataset: &str = "dataset/test.zip";
+    let file = File::open(dataset)?;
+    let mut archive = ZipArchive::new(file)?;
+
+    let filename_prefix: &str = "folder/";
+    for index in 0..archive.len() {
+        let mut data_file = archive.by_index(index)?;
+        if data_file.name().starts_with(filename_prefix) && data_file.name().ends_with(".json") {
+            let mut data = String::new();
+            data_file.read_to_string(&mut data)?;
+            let articles_vec: Vec<Article> = serde_json::from_str(&data)?;
+            for art in articles_vec {
+                println!(
+                    "Id: {} | Title: {} | Text: {}...",
+                    art.id,
+                    art.title,
+                    art.text.chars().next().unwrap()
+                );
+            }
+        }
+    }
+
+    Ok(())
 }
